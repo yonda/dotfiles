cff886e93aea98e30e76edbe7460c6d8778c3d4b
yonda <honda@33i.co.jp>
2015-08-09 18:32:59 +0900

    add link file

---
+++ dotfile_link.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -sf ~/dotfiles/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/dotfiles/Brewfile ~/Brewfile
