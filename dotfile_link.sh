bc74623193c6a347c0cf4905ab52572c3ebd2017
yonda <honda@33i.co.jp>
2015-08-30 18:33:19 +0900

    for local dotfiles

--- dotfile_link.sh
+++ dotfile_link.sh
@@ -4,3 +4,7 @@ ln -sf ~/dotfiles/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/dotfiles/Brewfile ~/Brewfile
+
+# NOTE まだ作ってない時だけ
+# touch ~/.gitconfig.private
+# touch ~/.zshrc.local
