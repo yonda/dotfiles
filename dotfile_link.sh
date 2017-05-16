62b047924dad29390f61e5c14322c555331f2d93
yonda <honda@33i.co.jp>
2017-05-16 15:40:19 +0900

    mac 移行した

--- dotfile_link.sh
+++ dotfile_link.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-ln -sf ~/dotfiles/dotfiles/gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/dotfiles/tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/dotfiles/vimrc ~/.vimrc
-ln -sf ~/dotfiles/dotfiles/zshrc ~/.zshrc
-ln -sf ~/dotfiles/dotfiles/Brewfile ~/Brewfile
+ln -sf /Users/honda/src/github.com/yonda/dotfiles/dotfiles/gitconfig ~/.gitconfig
+ln -sf /Users/honda/src/github.com/yonda/dotfiles/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf /Users/honda/src/github.com/yonda/dotfiles/dotfiles/vimrc ~/.vimrc
+ln -sf /Users/honda/src/github.com/yonda/dotfiles/dotfiles/zshrc ~/.zshrc
+ln -sf /Users/honda/src/github.com/yonda/dotfiles/dotfiles/Brewfile ~/Brewfile
 
 # NOTE まだ作ってない時だけ
 # touch ~/.gitconfig.private
